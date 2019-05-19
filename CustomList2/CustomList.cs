8f499ca7703fb96553aad750600d99048bd26982
mynameisfury <jonathanweatherall0@gmail.com>
2019-05-19 12:28:04 -0500

    init

---
+++ CustomList2/CustomList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList2
+{
+    class CustomList<T>
+    {
+        public void Add(T value)
+        {
+
+        }
+
+    }
+}
---
+++ CustomList2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
