ac807a1f6b24a72eb154d5d826c8e50f19afd768
mynameisfury <jonathanweatherall0@gmail.com>
2019-05-20 13:52:13 -0500

    added operator overloads

--- CustomList2/CustomList.cs
+++ CustomList2/CustomList.cs
@@ -10,6 +10,53 @@ namespace CustomList2
         {
 
         }
+        public void Remove(T value)
+        {
+
+        }
+        
+
+
+
+        public static CustomList<T> operator +(CustomList<T> list1, CustomList<T> list2)
+        {
+            CustomList<T> tempList = new CustomList<T>();
+            for (int i = 0; i > list1.Count; i++)
+            {
+                tempList.Add(list1[i]);
+            }
+            for (int i = 0; i > list2.Count; i++)
+            {
+                tempList.Add(list2[i]);
+            }
+            return tempList;
+
+        }
+
+        public static CustomList<T> operator -(CustomList<T> list1, CustomList<T> list2)
+        {
+            for (int i = 0; i > list1.Count; i++)
+            {
+                for (int j = 0; j > list2.Count; j++)
+                {
+                    if (list1[i] == list2[j])
+                    {
+                        list1.Remove[i];
+                    }
+                }
+            }
+            return list1;
+
+        }
+
+
+        public readonly int Count;
+
+        public CustomList()
+        {
+            Count = 0;
+        }
+
 
     }
 }
